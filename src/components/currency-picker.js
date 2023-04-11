5c49610aa463d1474bdb3345125443088c719bc3
Azuriyale <Azuriyale@users.noreply.github.com>
2023-04-11 17:23:43 +0800

    added currency carousel on dashboard

--- src/components/currency-picker.js
+++ src/components/currency-picker.js
@@ -1,49 +1,100 @@
-import React from "react";
-import { Box, Typography, useTheme } from "@mui/material";
+import React, { useState } from "react";
+import { Box, Typography, useTheme, Button } from "@mui/material";
+import moment from "moment";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Navigation } from "swiper";
+import "./css/swiper.css";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
 
 export default function CurrencyChoose() {
   const theme = useTheme();
+  const pages = [1, 2, 3, 4, 5, 6];
+  const [render, setRender] = useState(true);
+  const [swipePages, setSwipePages] = useState(pages);
+
+  const addCurrency = () => {
+    const last = swipePages.slice(-1)[0];
+    console.log(last);
+    const newList = [...swipePages, last + 1];
+    setSwipePages(newList);
+  };
+
   return (
     <Box sx={{ maxHeight: "200vh", width: "100%", marginBottom: "5vh" }}>
+      {/* CURRENCIES */}
       <Box
         sx={{
           width: "100%",
           height: "350px",
-          backgroundColor: "rgb(2,0,36)",
-          background:
-            "linear-gradient(120deg, rgba(2,0,36,1) 0%, rgba(18,27,131,1) 17%, rgba(25,39,174,1) 34%, rgba(20,103,194,1) 45%, rgba(17,144,206,1) 50%, rgba(14,173,215,1) 57%, rgba(13,159,219,1) 62%, rgba(11,173,224,1) 67%, rgba(10,198,229,1) 79%, rgba(6,178,243,1) 88%, rgba(0,212,255,1) 94%, rgba(0,212,255,1) 100%)",
-        }}
-      ></Box>
-
-      <Box
-        sx={{
-          backgroundColor: "white",
-          width: "90%",
-          height: "1000px",
-          borderRadius: "10px",
-          marginLeft: "auto",
-          marginRight: "auto",
-          marginTop: "-15vh",
-          boxShadow: "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px",
+          backgroundColor: "#121268",
+          marginTop: "5vh",
           display: "flex",
-          flexDirection: "column",
+          flexDirection: "row",
           alignItems: "center",
         }}
       >
+        {/* SWIPER */}
         <Box
           sx={{
-            display: "flex",
-            flexDirection: "row",
-            alignItems: "center",
-            width: "90%",
-            [theme.breakpoints.down("970")]: {
-              flexDirection: "column",
-            },
-            [theme.breakpoints.down("480")]: {
-              width: "100%",
-            },
+            width: "1400px",
+            height: "350px",
+            marginLeft: "30px",
           }}
-        ></Box>
+        >
+          {render ? (
+            <Swiper
+              slidesPerView={5}
+              spaceBetween={30}
+              navigation={true}
+              modules={[Pagination, Navigation]}
+              style={{
+                maxWidth: "1400px",
+                height: "262.5px",
+                paddingTop: "12vh",
+              }}
+              className="mySwiper"
+            >
+              {swipePages.map((p) => (
+                <SwiperSlide className="swiper-slide">Slide {p}</SwiperSlide>
+              ))}
+            </Swiper>
+          ) : null}
+        </Box>
+        <Box
+          sx={{
+            backgroundColor: "#007aff",
+            minWidth: "256px",
+            maxWidth: "256px",
+            minHeight: "150px",
+            maxHeight: "150px",
+            marginTop: "3vh",
+            marginLeft: "90px",
+            borderRadius: "10px",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            gap: "5px",
+          }}
+        >
+          {" "}
+          <Typography
+            sx={{
+              color: "white",
+              fontSize: "2em",
+              letterSpacing: "2px",
+            }}
+          >
+            Add Currency
+          </Typography>
+          <AddCircleOutlineIcon
+            sx={{ color: "white", fontSize: "3em" }}
+            onClick={() => addCurrency()}
+          />
+        </Box>
       </Box>
     </Box>
   );
